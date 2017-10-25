5ab702f11427244f0e0d5b2fdc1972f150881906
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2017-10-25 12:14:55 +0200

    test ramdom prob measure prior sampling

--- my-examples/test_rpm.jl
+++ my-examples/test_rpm.jl
@@ -1,9 +1,11 @@
 using Turing
 
-P1 = rand(NIGP(0.1, Normal()))
-# P2 = rand(NIGP(1.0, Normal(), true))
+# P1 = rand(NGGP(0.5, 1.0, Normal()))
+P1 = rand(NGGP(0.5, 1.0, Normal(), true))
+# P1 = rand(PYP(0.5, 1.0, Normal()))
+# P1 = rand(PYP(0.5, 1.0, Normal(), true))
 
-K = 100
+K = 1000
 N = 100
 size1 = zeros(K); size2 = zeros(K)
 
@@ -11,7 +13,7 @@ for k in 1:K
 println(k)
 res1 = zeros(N); res2 = zeros(N)
 for i in 1:N
-  println(i)
+  # println(i)
   res1[i] = rand(P1)
   # println(i)
   # res2[i] = rand(P2)
