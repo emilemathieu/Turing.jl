80e63eec1d1676d60f44deea045cca1dbd8d2c68
Adam Foster <a.evan.foster@gmail.com>
2017-10-26 17:27:25 +0100

    Add PYP sampling experiment

---
+++ my-examples/pyp_sampling_perf.jl
@@ -0,0 +1,33 @@
+using Turing
+# include("plot.jl")
+
+# Compare performance between (stochastic & unbounded) recursive style and
+# Sise-biased sampling style
+# /!\ DP2 and mLogBetaPK2 do not exist anymore; Go to src/models/rpm.jl
+# comment Distributions.rand(d::NormalizedRandomMeasure) & Distributions.rand(d::PoissonKingmanMeasure)
+# and uncomment Distributions.rand(d::DiscreteRandomProbabilityMeasure)
+
+alphas = [0.1 0.5 0.75]
+thetas = [1.0 10.0]
+samples = [1 10 100 1000]
+recursives = [true false]
+
+timings = []
+for recursive = recursives
+  for alpha = alphas
+    for theta = thetas
+      for sample_size = samples
+        sps = zeros(sample_size)
+        P = rand(PYP(alpha, theta, Normal(0, 1), recursive))
+        for i=1:sample_size
+          sps[i] = rand(P)
+        end
+        append!(timings, [(recursive, alpha, theta, sample_size, length(P.atoms))])
+      end
+    end
+  end
+end
+
+
+
+println(timings)
